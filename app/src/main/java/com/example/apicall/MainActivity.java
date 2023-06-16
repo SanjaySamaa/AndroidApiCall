52b12ae88f4222240ffefa4ff17ae6553a1745c8
SanjaySamaa <bistasanjay7@gmail.com>
2023-06-16 07:46:11 +0545

    api call added titles to lisview

--- app/src/main/java/com/example/apicall/MainActivity.java
+++ app/src/main/java/com/example/apicall/MainActivity.java
@@ -3,48 +3,83 @@ package com.example.apicall;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-public class MainActivity extends AppCompatActivity {
-    private String API_URL = "https://jsonplaceholder.typicode.com/posts/1";
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+//    private String API_URL = "https://jsonplaceholder.typicode.com/posts/1";
+    private List<String> titleResponse;
+    private String apiUrl = "https://jsonplaceholder.typicode.com/posts";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView tv1 = findViewById(R.id.tv1);
         TextView tv2 = findViewById(R.id.tv2);
+        ListView lv = findViewById(R.id.lv);
+        titleResponse = new ArrayList<>();
 
-        JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.GET, API_URL, null, new Response.Listener<JSONObject>() {
+
+//        JsonObjectRequest jsonObject = new JsonObjectRequest(Request.Method.GET, API_URL, null, new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject response) {
+//                Toast.makeText(MainActivity.this, "sucess" +response.toString(),Toast.LENGTH_SHORT).show();
+//                try {
+//                    tv1.setText(response.get("title").toString());
+//                } catch (JSONException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+
+//        }, new Response.ErrorListener() {
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Toast.makeText(MainActivity.this, "error here" +error.toString(), Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+//        Volley.newRequestQueue(this).add(jsonObject);
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, apiUrl, null, new Response.Listener<JSONArray>() {
             @Override
-            public void onResponse(JSONObject response) {
-                Toast.makeText(MainActivity.this, "sucess" +response.toString(),Toast.LENGTH_SHORT).show();
-                try {
-                    tv1.setText(response.get("title").toString());
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
+            public void onResponse(JSONArray response) {
+                Toast.makeText(MainActivity.this, "sucess"+response.toString(), Toast.LENGTH_SHORT).show();
+                for(int i = 0;i<response.length();i++){
+                    try {
+                        JSONObject jsonObject = response.getJSONObject(i);
+                        String title = jsonObject.getString("title");
+                        Log.d("API CALL", "onResponse: "+title);
+                        titleResponse.add(title);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
             }
-
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                Toast.makeText(MainActivity.this, "error here" +error.toString(), Toast.LENGTH_SHORT).show();
-
+                Toast.makeText(MainActivity.this, "something wrong"+error.toString(), Toast.LENGTH_SHORT).show();
             }
         });
-        Volley.newRequestQueue(this).add(jsonObject);
-
-
+        Volley.newRequestQueue(this).add(jsonArrayRequest);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(MainActivity.this, android.R.layout.simple_list_item_1,titleResponse);
+        lv.setAdapter(arrayAdapter);
     }
 }
